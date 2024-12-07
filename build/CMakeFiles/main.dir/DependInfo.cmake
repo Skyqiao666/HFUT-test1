a07eea3d0c7a2998c4c00e65e7d37cb978a855e3
Skyqiao666 <2971686898@qq.com>
2024-12-07 18:49:10 +0800

    Add files via upload

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/skyqiao/下载/CLASS_4/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/skyqiao/下载/CLASS_4/tasks/detector.cpp" "CMakeFiles/main.dir/tasks/detector.cpp.o" "gcc" "CMakeFiles/main.dir/tasks/detector.cpp.o.d"
+  "/home/skyqiao/下载/CLASS_4/tasks/plotter.cpp" "CMakeFiles/main.dir/tasks/plotter.cpp.o" "gcc" "CMakeFiles/main.dir/tasks/plotter.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
